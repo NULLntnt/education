974e895b57b36b3673434c1eacc01a2dd055e4b3
nullntnt <oraroamudaora@gmail.com>
2022-07-22 22:40:38 +0300

    JavaScript changes comitted

--- JavaScript/example.js
+++ JavaScript/example.js
@@ -1,1 +1,12 @@
+function printMax(Array){
+    let max = Array[0];
+    for(let i = 1; i < Array.length; i++){
+        if(Array[i] > max)
+            max = Array[i];
+    }
+    console.log("The maximum value in the array is: ", max);
+}
 
+
+arr = [2,35,6,7,132,7,0];
+printMax(arr);
